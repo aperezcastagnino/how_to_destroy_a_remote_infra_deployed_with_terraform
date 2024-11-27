d7af6c11b169a0a4063f9583c88ceb6f75ad2411
Pei <perezcastagnino.andres@gmail.com>
2024-11-27 16:16:45 -0300

    A few improvements

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-  echo "Path to terraform.tfstate is required (remotely, for example S3)"
-  exit 1
-fi
-
-export TF_BACKEND_KEY=$1
-
 source .env
-export TF_VAR_region TF_VAR_access_key TF_VAR_secret_key TF_BACKEND_BUCKET TF_BACKEND_KEY TF_BACKEND_ENCRYPT
+export TF_VAR_REGION
+export TF_VAR_ACCESS_KEY
+export TF_VAR_SECRET_KEY
+export TF_BACKEND_BUCKET
+export TF_TFSTATE_FILENAME
+export TF_BACKEND_ENCRYPT
+
 
 terraform init -reconfigure \
-  -backend-config="region=$TF_VAR_region" \
-  -backend-config="access_key=$TF_VAR_access_key" \
-  -backend-config="secret_key=$TF_VAR_secret_key" \
+  -backend-config="region=$TF_VAR_REGION" \
+  -backend-config="access_key=$TF_VAR_ACCESS_KEY" \
+  -backend-config="secret_key=$TF_VAR_SECRET_KEY" \
   -backend-config="bucket=$TF_BACKEND_BUCKET" \
-  -backend-config="key=$TF_BACKEND_KEY" \
+  -backend-config="key=$TF_TFSTATE_FILENAME" \
   -backend-config="encrypt=$TF_BACKEND_ENCRYPT"
 
-terraform destroy
+terraform destroy -var-file=terraform.tfvars
---
+++ terraform.example.tfvars
@@ -0,0 +1,4 @@
+account_id="123456789012"
+region="us-east-2"
+access_key="xxxxxxxxxxxxxxxxxxxx"
+secret_key="xxxxxxxxxxxxxxxxxxxxxxx"
